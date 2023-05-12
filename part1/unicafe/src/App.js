e4d446eb83aa0a5fdd4b6ed8b7a76d45fa47fe49
Pulkit <kpulkit15234@gmail.com>
2023-05-12 15:07:47 +0530

    p1q9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,22 +9,36 @@ const Buttons = (props) => {
   )
 }
 const Statistics = (props) => {
-  return(
-    <>
-      <p>{props.text} {props.value} </p>
-    </>
-  )
+  console.log(props.data)
+  if ( (props.data[0] + props.data[1] + props.data[2]) !==0){
+    //Variables
+    const tot = ( props.data[0]+ props.data[1]+ props.data[2])
+    const avg = ( props.data[0] -  props.data[2] ) / tot
+    const perc =  props.data[0] / tot
+    return(
+      <>
+        <p>{'Good ->'} {props.data[0]}</p>
+        <p>{'Neutral ->'} {props.data[1]}</p>
+        <p>{'Bad ->'} {props.data[2]}</p>
+        <p>{'All ->'} {tot}</p>
+        <p>{'Average ->'} {avg}</p>
+        <p>{'Positive ->'} {perc}</p>
+      </>
+    )
+  }
+  else{
+    return(
+        <p>
+          No feedback given
+        </p>
+    )
+  }
 }
 
 const App = () =>{
   const [Good , addGood] = useState(0)
   const [Neutral , addNeutral] = useState(0)
   const [Bad , addBad] = useState(0)
-
-  //Variables
-  const tot = (Good+Neutral+Bad)
-  const avg = (Good - Bad)/tot
-  const perc = Good/tot
 
   return(
     <>
@@ -35,17 +49,7 @@ const App = () =>{
         <Buttons text='bad' handleclick={()=>addBad(Bad+1)} />
       </div>
       <h1>Statistics</h1>
-      {/* <p>{'Good  ->'}  {Good}</p>
-      <p>{'Neutral  ->'}  {Neutral}</p>
-      <p>{'Bad  ->'}  {Bad}</p>
-      <p>{'Average  ->'}  {avg}</p>
-      <p>{'Positive  ->'}  {perc}</p> */}
-      <Statistics text={'Good ->'} value={Good} />
-      <Statistics text={'Neutral ->'} value={Neutral} />
-      <Statistics text={'Bad ->'} value={Bad} />
-      <Statistics text={'All ->'} value={tot} />
-      <Statistics text={'Average ->'} value={avg} />
-      <Statistics text={'Positive ->'} value={perc} />
+      <Statistics data={[Good,Neutral,Bad]} />
     </>
   )
 }
