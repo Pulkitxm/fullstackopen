78e0d0ac50a6fc760d8ef8caa7bad4132ee076db
Pulkit <kpulkit15234@gmail.com>
2023-05-12 14:09:11 +0530

    p1q7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -14,18 +14,28 @@ const App = () =>{
   const [Neutral , addNeutral] = useState(0)
   const [Bad , addBad] = useState(0)
 
+  //Variables
+  const tot = (Good+Neutral+Bad)
+  const avg = (Good - Bad)/tot
+  const perc = Good/tot
+  
   return(
     <>
       <h1>give feedack</h1>
       <div>
-        <Buttons text='good' handleclick={()=>addGood(Good+1)} />
+        <Buttons text='good' handleclick={()=>addGood(6)} />
+        <Buttons text='neutral' handleclick={()=>addNeutral(2)} />
+        <Buttons text='bad' handleclick={()=>addBad(1)} />
+        {/* <Buttons text='good' handleclick={()=>addGood(Good+1)} />
         <Buttons text='neutral' handleclick={()=>addNeutral(Neutral+1)} />
-        <Buttons text='bad' handleclick={()=>addBad(Bad+1)} />
+        <Buttons text='bad' handleclick={()=>addBad(Bad+1)} /> */}
       </div>
       <h1>Statistics</h1>
       <p>{'Good  ->'}  {Good}</p>
       <p>{'Neutral  ->'}  {Neutral}</p>
       <p>{'Bad  ->'}  {Bad}</p>
+      <p>{'Average  ->'}  {avg}</p>
+      <p>{'Positive  ->'}  {perc}</p>
     </>
   )
 }
