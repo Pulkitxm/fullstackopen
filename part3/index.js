636c248df38c75295b266143e53caf2d69c8b860
Pulkit <kpulkit15234@gmail.com>
2023-07-15 23:22:16 +0530

    p3q5

--- part3/index.js
+++ part3/index.js
@@ -1,4 +1,4 @@
-let notes = [
+let persons = [
     {
         "id": 1,
         "name": "Arto Hellas",
@@ -24,38 +24,62 @@ let notes = [
 const express = require("express")
 const app = express()
 
+app.use(express.json())
+
 app.get('/' , (req,res) =>{
     res.send("Hello")
 })
 
 app.get('/info' , (req,res) =>{
-    res.send(`<p>Phonebook has info for ${notes.length} people</p>
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
               <p>${new Date()}</p>
     `)
 })
 
 app.get('/api/persons',(req,res)=>{
-    res.send(notes)
+    res.send(persons)
 })
 
 app.get('/api/persons/:id',(req,res)=>{
     const id = Number(req.params.id);
-    const note = notes.filter(n=>n.id===id);
-    // if (note){
-    if (note.length===0){
+    const person = persons.filter(n=>n.id===id);
+    // if (person){
+    if (person.length===0){
         res.status(404).end()
     }else{
-        res.json(note);
+        res.json(person);
     }
     
 })
 
 app.delete('/api/persons/:id',(req,res)=>{
     const id = Number(req.params.id);
-    const note = notes.filter(note=>note.id===id);
-    notes = notes.filter(note=>note.id!==id);
-    console.log(note);
+    const person = persons.filter(person=>person.id===id);
+    persons = persons.filter(person=>person.id!==id);
+    console.log(person);
     res.status(204).end()
 })
+
+const generateId = () => Number((Math.random() * 10000000000000000).toFixed(0));
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+
+    res.json(person)
+});
 
 app.listen(3001)
