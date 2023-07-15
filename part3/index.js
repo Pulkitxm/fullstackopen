b0cf6bbb8110f704292c35ff1475603f97a3ad6d
Pulkit <kpulkit15234@gmail.com>
2023-07-15 21:42:20 +0530

    p3q1

---
+++ part3/index.js
@@ -0,0 +1,35 @@
+let notes = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+const express = require("express")
+const app = express()
+
+app.get('/' , (req,res) =>{
+    res.send("Hello")
+})
+
+app.get('/api/persons',(req,res)=>{
+    res.send(notes)
+})
+
+app.listen(3001)
