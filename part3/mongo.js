d3d1cc128ccc0955971b33fdff5d6b49ca6adbb3
Pulkit <kpulkit15234@gmail.com>
2023-07-25 15:46:19 +0530

    p3q12 connecting mongodb

---
+++ part3/mongo.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+    console.log('invalid request')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+// gwHxC6yyacED8MjM
+const url =
+    `mongodb+srv://pulkitxm:${ password }@cluster0.nyotmfb.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+const personSchema = new mongoose.Schema({
+    id: String,
+    name: String,
+    number: String
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+const person = new Person({
+    id:name+number,
+    name:name,
+    number:number
+})
+
+person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+})
+
+Person.find({}).then(result => {
+    result.forEach(person => {
+        console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+})
