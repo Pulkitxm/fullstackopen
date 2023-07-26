3e0251b7dce6982355f6b537f9b326ed1bdf7677
Pulkit <kpulkit15234@gmail.com>
2023-07-26 22:15:31 +0530

    p3q16 error handling

--- part3/index.js
+++ part3/index.js
@@ -29,6 +29,16 @@ require('dotenv').config()
 
 const Person = require('./models/Person')
 
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+
 // morgan(function (tokens, req, res) {
 //     return [
 //         tokens.method(req, res),
@@ -47,23 +57,24 @@ app.use(morgan(':method :url :status :response-time ms - :req-body'));
 
 const url = process.env.url;
 
-// app.get('/' , (req,res) =>{
+// app.get('/' , (req,res,next) =>{
 //     res.send("Hello")
 // })
 
-app.get('/info' , (req,res) =>{
+app.get('/info' , (req,res,next) =>{
     res.send(`<p>Phonebook has info for ${persons.length} people</p>
               <p>${new Date()}</p>
     `)
 })
 
-app.get('/api/persons',(req,res)=>{
+app.get('/api/persons',(req,res,next)=>{
     Person.find({}).then(person=>{
         res.json(person)
-    })
+    }).catch(error => next(error))
+
 })
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response,next) => {
     Person.findById(request.params.id)
         .then(person => {
             if (person) {
@@ -80,10 +91,7 @@ app.delete('/api/persons/:id', (request, response,next) => {
         .then(result => {
             response.status(204).end()
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
 app.put('/api/person/:id', (request, response,next) => {
@@ -100,10 +108,7 @@ app.put('/api/person/:id', (request, response,next) => {
         .then(updatedPerson => {
             response.json(updatedPerson)
         })
-        .catch(error => {
-            console.log(error)
-            response.status(400).send({ error: 'malformatted id' })
-        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -144,6 +149,8 @@ app.post('/api/persons', (req, res) => {
     }
     // console.log(persons);
 });
+
+app.use(errorHandler)
 
 const PORT = process.env.port
 app.listen(PORT, () => {
