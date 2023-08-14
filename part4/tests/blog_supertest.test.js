c543d910158f2b1ac3e2ce488a090f87ed168ce2
= <=>
2023-08-14 20:43:15 +0530

    p4q9 verify id prop

--- part4/tests/blog_supertest.test.js
+++ part4/tests/blog_supertest.test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
+const blog = require('../models/blog')
 
 const api = supertest(app)
 
@@ -9,6 +10,14 @@ test('blogs are returned as json', async () => {
         .get('/api/blogs')
         .expect(200)
         .expect('Content-Type', /application\/json/)
+})
+
+test('id is defined for each blog', async () => {
+    const blogs = await api.get('/api/blogs')
+    blogs.body.forEach(blog => {
+        // console.log(blog);
+        expect(blog.likes).toBeDefined();
+    });
 })
 
 afterAll(async () => {
