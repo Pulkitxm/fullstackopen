d4c109b31326a74c52541b8f5770d8a47c0feabb
= <=>
2023-08-14 19:41:54 +0530

    p4q8 supertest [ return as json ]

---
+++ part4/tests/blog_supertest.test.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+test('blogs are returned as json', async () => {
+    await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+})
+
+afterAll(async () => {
+    await mongoose.connection.close()
+})
