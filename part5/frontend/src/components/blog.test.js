eebe4aac8f850534cc59e02aba8598d74fe90a50
Pulkit <kpulkit15234@gmail.com>
2023-09-13 14:46:07 +0530

    p5q14 button click to show likes and url

--- part5/frontend/src/components/blog.test.js
+++ part5/frontend/src/components/blog.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import {screen,render} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe("<Blogs/>",()=>{
@@ -19,6 +20,26 @@ describe("<Blogs/>",()=>{
         expect(container).not.toHaveTextContent(blog.author)
         expect(container).not.toHaveTextContent(blog.likes)
         expect(container).not.toHaveTextContent(blog.url)
+    })
+
+    test("blog renders likes,author and url after button click",async ()=>{
+        const blog = {
+            title: "Test Blog",
+            author: "64ff5c5f30c256a7c7c67827",
+            url: "link_to_blog",
+            likes: 0,
+            id: "unique_id"
+        };
+
+        const mock = jest.fn;
+
+        const {container} =  render(<Blog blog={blog} handleDelete={mock}  />);
+        const user = userEvent.setup();
+        const button = screen.getByText("Delete");
+        await user.click(button);
+        expect(container).toHaveTextContent(blog.author)
+        expect(container).toHaveTextContent(blog.likes)
+        expect(container).toHaveTextContent(blog.url)
     })
 
 })
