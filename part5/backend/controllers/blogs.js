e43562e82c6a794c49f1b7790d6d6dd788c1ca10
Pulkit <kpulkit15234@gmail.com>
2023-10-03 23:53:20 +0530

    p5q19 logged-in user can create a new blog

--- part5/backend/controllers/blogs.js
+++ part5/backend/controllers/blogs.js
@@ -35,8 +35,9 @@ const getTokenFrom = request => {
 }
 
 blogsRouter.post('/api/blogs', async (request, response, next) => {
+    console.log(request);
     const body = request.body
-    console.log(body);
+    // console.log(body);
     const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
     if (!decodedToken.id) {
         return response.status(401).json({ error: 'token invalid' })
--- part5/frontend/cypress/e2e/blog.cy.js
+++ part5/frontend/cypress/e2e/blog.cy.js
@@ -1,53 +1,93 @@
 describe('blog app', () => {
+  beforeEach(() => {
+    cy.request('POST', `${Cypress.env('backendUrl')}/api/testing/reset`);
+    cy.visit('');
+  });
 
-  beforeEach(()=>{
-    cy.request('POST', `${Cypress.env('backendUrl')}/api/testing/reset`)
-    cy.visit('')    
-  })
-  
   it('displays the login form by default', () => {
-    cy.contains('Login').click()
-    
-    //ensuring that form elements are displayed
-    cy.contains('username')
-    cy.contains('password')
-    cy.contains('login')
-  })
-  
-  describe("after login",()=>{
-    
-    it("login is successful with correct credentials",()=>{
+    cy.contains('Login').click();
+    cy.contains('username');
+    cy.contains('password');
+    cy.contains('login');
+  });
+
+  describe('login attempts', () => {
+    it('login is successful with correct credentials', () => {
       const user = {
         name: 'Pulkit',
         username: 'pulkit',
-        password: 'pulkit123'
-      }
-      cy.request('POST', `${Cypress.env('backendUrl')}/api/users`, user).then(res=>{
-        window.localStorage.setItem(
-          'loggedNoteappUser', JSON.stringify(res.body)
-        )
-        cy.visit('')    
-      })
-      cy.contains("Pulkit logged in")   
-    })
+        password: 'pulkit123',
+      };
+      cy.request('POST', `${Cypress.env('backendUrl')}/api/users`, user).then((res) => {
+        window.localStorage.setItem('loggedNoteappUser', JSON.stringify(res.body));
+        cy.visit('');
+      });
+      cy.contains('Pulkit logged in');
+    });
 
-    it("login is unsuccessful with wrong credentials", () => {
+    it('login is unsuccessful with wrong credentials', () => {
       const user = {
         name: 'wrong',
         username: 'wrong',
-        password: 'wrong'
-      }
-    
-      
+        password: 'wrong',
+      };
       cy.request({
         method: 'POST',
         url: `${Cypress.env('backendUrl')}/api/login`,
         body: user,
-        failOnStatusCode: false 
+        failOnStatusCode: false,
       }).then((response) => {
-        expect(response.status).to.be.oneOf([400, 401, 403, 404]); 
+        expect(response.status).to.be.oneOf([400, 401, 403, 404]);
       });
-      
     });
-  })
-})
+  });
+
+  describe('after login', () => {
+    beforeEach(() => {
+      const user = {
+        name: 'Pulkit',
+        username: 'pulkit',
+        password: 'pulkit',
+      };
+      cy.request('POST', `${Cypress.env('backendUrl')}/api/users`, user).then((res) => {
+        window.localStorage.setItem('loggedNoteappUser', JSON.stringify(res.body));
+        cy.visit('');
+      });
+    });
+
+    it('passed', () => {
+      cy.contains('Add a new Note').click();
+      cy.get("input[name='Username']:eq(0)").type('Cypress');
+      cy.get("input[name='Username']:eq(1)").type(JSON.parse(window.localStorage.loggedNoteappUser).id);
+      cy.get("input[name='Username']:eq(2)").type('https://devpulkit.vercel.app/');
+
+      const user = {
+        username: 'pulkit',
+        password: 'pulkit',
+      };
+
+      cy.request('POST', `${Cypress.env('backendUrl')}/api/login`, user).then((res) => {
+        const blog = {
+          title: 'Cypress',
+          author: `${JSON.parse(window.localStorage.loggedNoteappUser).id}`,
+          url: 'https://devpulkit.vercel.app/',
+        };
+
+        cy.request({
+          method: 'POST',
+          url: `${Cypress.env('backendUrl')}/api/blogs`,
+          body: blog,
+          headers: {
+            Authorization: `Bearer ${res.body.token}`,
+            'Content-Type': 'application/json',
+          },
+        });
+
+        cy.visit('');
+        cy.contains('view').click();
+        cy.contains('Cypress');
+        cy.contains('https://devpulkit.vercel.app/');
+      });
+    });
+  });
+});
