25c6d79b71570a04c1eb3f9a4a8116a95e170efa
Pulkit <kpulkit15234@gmail.com>
2023-10-03 21:02:04 +0530

    p5q18 successful and unsuccessful login attempts

--- part5/frontend/cypress.config.js
+++ part5/frontend/cypress.config.js
@@ -5,5 +5,9 @@ export default defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl:"http://localhost:5173/"
   },
+  env:{
+    backendUrl:"http://localhost:3003",
+  }
 });
--- part5/frontend/cypress/e2e/blog.cy.js
+++ part5/frontend/cypress/e2e/blog.cy.js
@@ -1,17 +1,53 @@
-describe('before login', () => {
+describe('blog app', () => {
 
   beforeEach(()=>{
-    cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:5173/')    
+    cy.request('POST', `${Cypress.env('backendUrl')}/api/testing/reset`)
+    cy.visit('')    
   })
-
+  
   it('displays the login form by default', () => {
     cy.contains('Login').click()
-
+    
     //ensuring that form elements are displayed
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
   })
   
+  describe("after login",()=>{
+    
+    it("login is successful with correct credentials",()=>{
+      const user = {
+        name: 'Pulkit',
+        username: 'pulkit',
+        password: 'pulkit123'
+      }
+      cy.request('POST', `${Cypress.env('backendUrl')}/api/users`, user).then(res=>{
+        window.localStorage.setItem(
+          'loggedNoteappUser', JSON.stringify(res.body)
+        )
+        cy.visit('')    
+      })
+      cy.contains("Pulkit logged in")   
+    })
+
+    it("login is unsuccessful with wrong credentials", () => {
+      const user = {
+        name: 'wrong',
+        username: 'wrong',
+        password: 'wrong'
+      }
+    
+      
+      cy.request({
+        method: 'POST',
+        url: `${Cypress.env('backendUrl')}/api/login`,
+        body: user,
+        failOnStatusCode: false 
+      }).then((response) => {
+        expect(response.status).to.be.oneOf([400, 401, 403, 404]); 
+      });
+      
+    });
+  })
 })
