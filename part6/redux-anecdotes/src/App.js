2b006a3e447ea3c73cf1543e4b12f98f987c12e3
Pulkit <kpulkit15234@gmail.com>
2023-10-10 00:05:13 +0530

    p6q14 fetch the anecdotes from the backend

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -2,8 +2,24 @@ import NewAnectdote from './components/NewAnecdote'
 import DisplayAnectdote from './components/DisplayAnecdote'
 import VisibilityFilter from './components/VisibilityFilter'
 import Notification from './components/Notification'
+import { useEffect } from 'react'
 
-const App = () => {  
+import anecdoteService from './services/anecdote'
+import { useDispatch } from 'react-redux'
+
+const App = () => { 
+
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    anecdoteService.getAll().then((res)=>{
+      console.log(res)
+      res.forEach(i => {
+        dispatch({ type: 'anecdote/addAnectode', payload: i.content })
+      });
+    })
+  },[])
+   
   return (
     <div>
       <Notification/>
