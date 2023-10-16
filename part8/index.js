d8261b44cd0c6c72b93b233eafb1d0665c6bba2c
Pulkit <kpulkit15234@gmail.com>
2023-10-16 22:07:19 +0530

    p8q2 query allBooks

--- part8/index.js
+++ part8/index.js
@@ -97,13 +97,15 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () =>books.length,
-    authorCount: () =>authors.length
+    authorCount: () =>authors.length,
+    allBooks: ()=>books
   }
 }
 
