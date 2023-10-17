e9edf24715013573e7d4f5cea382fe9f78c68679
Pulkit <kpulkit15234@gmail.com>
2023-10-17 23:11:07 +0530

    p8q12 birth year can be set only for an existing author

--- part8/library-frontend/src/components/AuthorForm.js
+++ part8/library-frontend/src/components/AuthorForm.js
@@ -1,29 +1,37 @@
 import React, { useState } from 'react'
+import Select from 'react-select';
 
 import { editAuthor,fetchAuthors } from '../gqlQueries'
-import { useMutation } from '@apollo/client'
+import { useMutation, useQuery } from '@apollo/client'
 
 const AuthorForm = () => {
   
-  const [name, setname] = useState("")
   const [year, setyear] = useState("")
+  const [selectedOption, setSelectedOption] = useState(null);
 
   const [alterAuthor] = useMutation(editAuthor,{
     refetchQueries:[ {quer:fetchAuthors} ]
   })
+
+  const authors = useQuery(fetchAuthors).data.allAuthors.map(author=>({ value:author.name , label:author.name }))
   
   const handleUpdate = () => {
-    alterAuthor({variables:{name,setBornTo:parseInt(year)}})
-    setname("")
+    alterAuthor({variables:{name:selectedOption.value,setBornTo:parseInt(year)}})
     setyear("")
   }
   
   return (
     <div>
       <h2>Set Birth year</h2>
-      name<input type="text" value={name} onChange={e=>setname(e.target.value)} />
+      {/* name<input type="text" value={name} onChange={e=>setname(e.target.value)} /> */}
+      <Select
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={authors}
+      />
       <br />
       born<input type="text" value={year} onChange={e=>setyear(e.target.value)} />
+      <br />
       <br />
       <button onClick={handleUpdate} >update author</button>
     </div>
