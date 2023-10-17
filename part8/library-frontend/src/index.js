62aedf517a8698959f5ceb561b0ec02790571acc
Pulkit <kpulkit15234@gmail.com>
2023-10-17 22:17:30 +0530

    fixing minor issues

--- part8/library-frontend/src/index.js
+++ part8/library-frontend/src/index.js
@@ -2,4 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache
+} from '@apollo/client'
+
+const client = new ApolloClient({
+    uri:'http://localhost:4000',
+    cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+)
